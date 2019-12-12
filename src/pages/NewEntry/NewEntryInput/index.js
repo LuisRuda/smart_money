fa527ae6c6572cfd6a45590c2883104e9a31a262
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-12-12 18:45:24 -0300

    Button to enter negative or positive value

--- src/pages/NewEntry/NewEntryInput/index.js
+++ src/pages/NewEntry/NewEntryInput/index.js
@@ -2,25 +2,27 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import {TextInputMask} from 'react-native-masked-text';
-import {Container} from './styles';
+import {Container, DebitButton, DebitButtonText} from './styles';
 
 import colors from '~/assets/colors';
 
 const styles = StyleSheet.create({
   input: {
+    flex: 1,
     fontSize: 28,
     color: colors.white,
-    textAlign: 'center',
-    backgroundColor: colors.asphalt,
-    borderRadius: 15,
-    marginHorizontal: 20,
-    marginVertical: 10,
+    textAlign: 'right',
+    paddingRight: 20,
   },
 });
 
 export default function NewEntryInput({value, onChangeValue}) {
   return (
     <Container>
+      <DebitButton>
+        <DebitButtonText>-</DebitButtonText>
+        <DebitButtonText>R$</DebitButtonText>
+      </DebitButton>
       <TextInputMask
         style={styles.input}
         type="money"
--- src/pages/NewEntry/NewEntryInput/styles.js
+++ src/pages/NewEntry/NewEntryInput/styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import colors from '~/assets/colors';
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background-color: ${colors.asphalt};
+  border-radius: 15px;
+  margin: 10px 20px;
+`;
+
+export const DebitButton = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 20px;
+`;
+
+export const DebitButtonText = styled.Text`
+  color: ${colors.white};
+  font-size: 28px;
+`;
