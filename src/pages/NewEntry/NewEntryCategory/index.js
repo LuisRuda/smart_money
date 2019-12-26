044d8b1750a1e7ed03e57952f869937bae1084a9
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-12-26 11:40:35 -0300

    Loading categories in modal

--- src/pages/NewEntry/NewEntryCategory/index.js
+++ src/pages/NewEntry/NewEntryCategory/index.js
@@ -1,9 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Modal} from 'react-native';
-import {Container, PickerButton, PickerButtonText} from './styles';
+import {
+  Container,
+  PickerButton,
+  PickerButtonText,
+  ContainerModal,
+  List,
+  ModalItemText,
+  CloseButton,
+  CloseButtonText,
+} from './styles';
+
+import {getAllCategories} from '~/services/Categories';
 
 export default function NewEntryCategory() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const data = await getAllCategories();
+      setAllCategories(data);
+    }
+
+    loadCategories();
+  }, []);
 
   return (
     <Container>
@@ -11,9 +32,20 @@ export default function NewEntryCategory() {
         <PickerButtonText>Alimentação</PickerButtonText>
       </PickerButton>
       <Modal animationType="slide" transparent={false} visible={modalVisible}>
-        <PickerButton onPress={() => setModalVisible(false)}>
-          <PickerButtonText>Fechar</PickerButtonText>
-        </PickerButton>
+        <ContainerModal>
+          <List
+            data={allCategories}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <PickerButton>
+                <ModalItemText color={item.color}>{item.name}</ModalItemText>
+              </PickerButton>
+            )}
+          />
+          <CloseButton onPress={() => setModalVisible(false)}>
+            <CloseButtonText>Fechar</CloseButtonText>
+          </CloseButton>
+        </ContainerModal>
       </Modal>
     </Container>
   );
--- src/pages/NewEntry/NewEntryCategory/styles.js
+++ src/pages/NewEntry/NewEntryCategory/styles.js
@@ -16,3 +16,32 @@ export const PickerButtonText = styled.Text`
   color: ${colors.white};
   text-align: center;
 `;
+
+export const ContainerModal = styled.View`
+  flex: 1;
+  background-color: ${colors.background};
+`;
+
+export const List = styled.FlatList``;
+
+export const ModalItemText = styled.Text`
+  font-size: 22px;
+  color: ${props => props.color};
+  text-align: center;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  background-color: ${colors.background};
+  border-color: ${colors.green};
+  border-width: 1px;
+  border-radius: 15px;
+  margin-vertical: 10px;
+  padding: 5px 20px;
+  align-self: center;
+`;
+
+export const CloseButtonText = styled.Text`
+  font-size: 14px;
+  color: ${colors.green};
+  text-align: center;
+`;
